61a2f40596edb15f699cc8c65e3a449dab4f3b8b
Code Wizard <codewizard@gmail.com>
2025-09-25 17:10:32 +0300

    binary_search

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+function binarySearch(bet1, bet2) {
+  let left = 0
+  let right = bet1.length - 1
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    if (bet1[mid] === bet2) return mid
+    if (bet1[mid] < bet2) left = mid + 1
+    else right = mid - 1
+  }
+  return -1
+}
+
+const numbers = [1, 3, 5, 7, 9, 11, 15]
+const index = binarySearch(numbers, 7)
+console.log(index)
